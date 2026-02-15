bbcd0b7657cb262fbd1ce5d8f0b9a270a526f5f2
Francesco Calabria <clbfnc02m30g317v@studenti.unical.it>
2026-02-15 19:53:17 +0100

    nuovo logo e schermata di benvenuto

---
+++ app/src/main/java/com/example/bookey/WelcomeActivity.java
@@ -0,0 +1,24 @@
+package com.example.bookey;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_welcome);
+
+        Button enterButton = findViewById(R.id.enterButton);
+        enterButton.setOnClickListener(v -> {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        });
+    }
+}
